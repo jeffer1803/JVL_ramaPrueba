a01ce8733a6cd5f9dee59b73b4059177eecf40b9
jeffer1803 <vanegasjefferson02@gmail.com>
2025-11-24 15:03:58 -0500

    laravel 1

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-route :: get ()
+route :: get ("/", function () {
+    return "<h1>hola mundo</h1>";
+});
+
+route :: get ("/contacto", function () {
+    return "HOLA DESDE LA PAGINA DE CONTACTO";
+}); 
